eea319c7d148bc853ecfd20974ac3f2af5c80edc
Erika Gibbons <overkillobsidian@gmail.com>
2024-03-22 11:39:10 -0400

    new size for img

---
+++ Solar System.js
@@ -0,0 +1,72 @@
+function earthGif(){
+     document.getElementById("earth").src="earth_gif.gif" ;
+}
+
+
+function earthPic(){
+     document.getElementById("earth").src="earth.jpg";
+}
+
+function mercuryGif(){
+     document.getElementById("mercury").src="mercury_gif.gif";
+}
+
+function mercuryPic(){
+     document.getElementById("mercury").src="mercury.jpg";
+}
+
+function venusGif(){
+     document.getElementById("venus").src="venus_gif.gif";
+}
+
+function venusPic(){
+     document.getElementById("venus").src="venus.jpg";
+}
+
+function marsGif(){
+     document.getElementById("mars").src="mars_gif.gif";
+}
+
+function marsPic(){
+     document.getElementById("mars").src="Mars.jpg";
+}
+
+function jupiterGif(){
+     document.getElementById("jupiter").src="Jupiter_gif.gif";
+}
+
+function jupiterPic(){
+     document.getElementById("jupiter").src="Jupiter.jpg";
+}
+
+function saturnGif(){
+     document.getElementById("saturn").src="saturn_gif.gif";
+}
+
+function saturnPic(){
+     document.getElementById("saturn").src="saturn.jpg";
+}
+
+function uranusGif(){
+     document.getElementById("uranus").src="uranus_gif.gif";
+}
+
+function uranusPic(){
+     document.getElementById("uranus").src="uranus.png";
+}
+
+function neptuneGif(){
+     document.getElementById("neptune").src="neptune_gif.gif";
+}
+
+function  neptunePic(){
+     document.getElementById("neptune").src="Neptune.jpg";
+}
+
+function plutoGif(){
+     document.getElementById("pluto").src="pluto_gif.gif";
+}
+
+function plutoPic(){
+     document.getElementById("pluto").src="pluto.jpg";
+}
